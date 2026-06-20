4af07881f655c9202bcb93b94eef92ffb6a36874
manivannancsmca <manivannancsmca@gmail.com>
2026-06-20 15:50:09 +0530

    first commit

---
+++ src/main/java/com/spring_cicd_reusable_workflow/SpringCicdReusableWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.spring_cicd_reusable_workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdReusableWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCicdReusableWorkflowApplication.class, args);
+	}
+
+}
